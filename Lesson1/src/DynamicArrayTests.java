4c496ef6b4f20f8df9f5116ce9d4801708919afb
Neykuratick <neykuratick@yahoo.com>
2021-02-19 08:07:20 +0300

    before import

---
+++ Lesson1/src/DynamicArrayTests.java
@@ -0,0 +1,12 @@
+
+
+public class DynamicArrayTests {
+    DynamicArray dArr = new DynamicArray();
+
+
+    public void init_DefaultArraySize_5() {
+        DynamicArray dArr = new DynamicArray();
+        int dArrLength = dArr.getLength();
+
+    }
+}
--- Lesson1/src/Test.java
+++
@@ -1,3 +0,0 @@
-public class Test {
-    DynamicArray dArr = new DynamicArray();
-}
