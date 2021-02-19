a6d2cb1c1bdd882a2009673e60d3619307fa39a6
Neykuratick <neykuratick@yahoo.com>
2021-02-19 07:21:07 +0300

    multiple array types

--- Lesson1/src/DynamicArray.java
+++ Lesson1/src/DynamicArray.java
@@ -1,13 +1,15 @@
-public class DynamicArray {
-    private int[] data;
-    int DEFAULT_SIZE = 5;
+public class DynamicArray<T> {
+    private T[] data;
+    private int DEFAULT_SIZE = 5;
+    private int size;
 
     public DynamicArray() {
-        data = new int[DEFAULT_SIZE];
+        data = (T[]) new Object[DEFAULT_SIZE];
     }
 
     public DynamicArray(int size) {
-        data = new int[size];
+        data = (T[]) new Object[size];
+        this.size = size;
     }
 
     public int getLength() {
--- Lesson1/src/Main.java
+++ Lesson1/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]) {
-        DynamicArray dArr = new DynamicArray(10); //
+        DynamicArray dArr = new DynamicArray(10);
 
         System.out.println(dArr.getLength());
     }
