22d358f3017d73726ef02bae6ff521100e0be376
Neykuratick <neykuratick@yahoo.com>
2021-03-05 08:03:50 +0300

    append

--- Lesson1/src/DLinkedListTests.java
+++ Lesson1/src/DLinkedListTests.java
@@ -94,4 +94,57 @@ public class DLinkedListTests extends Assert {
     }
 
     // -- /push --
+
+    // -- append --
+
+    @Test
+    public void append_listSize() {
+        // positive test
+
+        DLinkedList list = new DLinkedList();
+
+        list.append(1);
+
+        int actual = list.size;
+        int expected = 1;
+        assertEquals(actual, expected);
+
+        list.append(1);
+        list.append(1);
+        list.append(1);
+
+        actual = list.size;
+        expected = 4;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void append_value_proof() {
+        // positive test
+
+        DLinkedList list = new DLinkedList();
+
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+
+        Object expected = 1;
+        Object actual = list.getValueIndex(0);
+        assertEquals(actual, expected);
+
+        expected = 2;
+        actual = list.getValueIndex(1);
+        assertEquals(actual, expected);
+
+        expected = 3;
+        actual = list.getValueIndex(2);
+        assertEquals(actual, expected);
+
+        expected = 4;
+        actual = list.getValueIndex(3);
+        assertEquals(actual, expected);
+    }
+
+    // -- /append --
 }
